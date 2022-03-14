955b202fa174c315e0a0115f62223bbdd42be9e9
Felix Lindblom <74712636+Felix-lindblom@users.noreply.github.com>
2022-03-14 21:43:14 +0100

    0.3.0 deck start
    Började med min deck nu på javascript

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+// Antal bilder du har skriv här in
+let decksizepair = "12"
+
+function memodeck(){
+    let deck = new Array();
+    for(let i = 0; i < decksizepair; i++){
+        for(let x = 0; x < 2; x++){
+
+            let memocard = {imgnum: i + 1, cardnum: 2 * decksizepair - i }
+            // tror detta skapar ett unikt kort för varsit och unkit tal. 
+            // Kan krävas gör om matten där jag vet ej om flertal matte i js funkar.
+            
+
+        }
+
+    }
+}
