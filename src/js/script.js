f5ca7f50b956acfad6492bdad7f12af4454f8c6b
Felix Lindblom <74712636+Felix-lindblom@users.noreply.github.com>
2022-03-17 22:13:12 +0100

    0.6.0 får fan knappt in i html
    vet ej hur man får inte kod riktigt men nu kommer bilder in fall.

--- src/js/script.js
+++ src/js/script.js
@@ -12,15 +12,14 @@ const deck = [];
                 cardnum: (decksizepair*j) - i +1
             };
 
-           
-
             deck.push(memocard)
+
         }
         
         
     }
 //  console.log(deck), stämmer blir varan så ser lite udda men det innebär det är redan blandat med varan
-console.log(deck)
+
 
 
     for (let i = 0; i <= 10*deck.length; i++){
@@ -35,20 +34,44 @@ console.log(deck)
     }
 
 
-    // mer här med få det till rätt format och sånt
-function inputDeckHTML (deck){
 
-    // kort består en <li> som sedan har en <img> i sig.
-    document.getElementById("playarea").innerHTML = "";
 
-    for(let i = 0; i<deck.length;i++ ){
-        let memocard = document.createElement("li");
-        memocard.className = cardnum
-        // Vill inom li skapa en <img> som använder bild för baksida, sedan ska en senare funktion byta ut den till imgnum. Men går att testa med imgnum till en början
-        // tror classnamet är fel 
-    }    
 
-}
+    function display() {
+
+        for(let i = 0; i<=deck.length;i++){
+            var pl = document.querySelector('.playarea');
+
+            // börjar bli sent nu...
+            // får inte till att skapa <li>, för jag vill gör en li som har bilden i sig. Li ska hålla cardnum även. 
+ 
+            // var cardli = document.createElement("li");
+            // cardli.setAttribute("${i}");
+            // pl.appendChild(cardli);
+
+            var pic = document.createElement("img");
+            pic.src = "img/minesweeper-img/bak.png";
+            pl.appendChild(pic);
+        
+        
+        }
+        
+
+    }
+    display()
+
+
+    // jag tänkte göra om man clicka byter man ut bak.png till matchade bild till cardnum. 
+    // Dock vet jag ej om den är dålig id med man inte kan flippa. 
+    
+
+
+   
+
+
+
+
+
 
 
 // skapa en vändare function
